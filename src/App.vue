8c3a2ea80cbb3ef0a15859ed515d31aad212fe65
고태경 <202244042@itc.ac.kr>
2024-10-27 12:45:01 +0900

    폰트어썸 라이브러리 추가 및 인적사항 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <template>
   <div>
-    <!-- 디렉티브에 문자열 값을 직접 전달합니다 -->
-    <h1 v-typing="'책임감 있는 데이터 분석가 고태경입니다.'" data-delay="200" class="typing-text"></h1>
+    <header :class="{ scrollDown: isScrollDown }" class="header">
+      <h1 v-typing="'책임감 있는 데이터 분석가 고태경입니다.'" data-delay="200" class="typing-text"></h1>
+    </header>
+    <main class="main-content">
+      <div class="intro-section">
+        <img src="@/assets/증명사진.jpg" alt="Profile Image" class="profile-image">
+        <div class="intro-text">
+          <h2>| INTRODUCTION</h2>
+          <div class="contact-info">
+            <font-awesome-icon :icon="['fas', 'venus']" />
+            <p>고태경</p>
+          </div>
+
+          <div class="contact-info">
+            <font-awesome-icon :icon="['fas', 'cake-candles']" />
+            <p>2004.02.25 (만 20세)</p>
+          </div>
+          <div class="contact-info">
+            <font-awesome-icon :icon="['fas', 'phone']" />
+            <p>010-2987-5082</p>
+          </div>
+          <div class="contact-info">
+            <font-awesome-icon :icon="['fas', 'envelope']" />
+            <p>202244042@itc.ac.kr</p>
+          </div>
+        </div>
+      </div>
+      <p>여기에 포트폴리오 콘텐츠가 들어갑니다.</p>
+    </main>
   </div>
 </template>
 
@@ -27,21 +54,31 @@ export default {
         typeWriter();
       }
     }
+  },
+  data() {
+    return {
+      isScrollDown: false, // 스크롤 상태를 관리하는 변수
+      prevScrollTop: 0
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const nextScrollTop = window.pageYOffset || 0;
+      if (nextScrollTop > this.prevScrollTop) {
+        // 스크롤 다운
+        this.isScrollDown = true;
+      } else if (nextScrollTop < this.prevScrollTop) {
+        // 스크롤 업
+        this.isScrollDown = false;
+      }
+      this.prevScrollTop = nextScrollTop;
+    }
   }
 }
 </script>
-
-<style>
-body {
-  background-color: #ffcdcd; /* 전체 배경색 설정 */
-}
-
-.typing-text {
-  text-align: center; /* 텍스트 중앙 정렬 */
-  display: block;
-  margin: 0 auto; /* 중앙 정렬 유지, 상단 마진 제거 */
-  font-size: 2em; /* 글자 크기 */
-  background-color: #ffb3b3; /* 헤더 배경색 */
-  padding: 10px; /* 텍스트 주변 여백 */
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import App from './App.vue'
 import './assets/styles.css';
 import typingEffect from './directives/typingEffect'
 
+// 폰트 어썸
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+
+// 아이콘 라이브러리에 'fas' 아이콘 추가
+library.add(fas);
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon); // 폰트어썸
 app.directive('typing', typingEffect)
 app.mount('#app')
